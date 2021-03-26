7370f84e2260b7fbc339368b31d7b6f1c706f0ab
Bokang Manche <bokangmogaladi@gmail.com>
2021-03-26 16:49:01 +0200

    initial commit

---
+++ JSONPlaceholder/src/test/java/VerifyCustomerTest.java
@@ -0,0 +1,24 @@
+public class VerifyCustomerTest {
+
+    int custId;
+    String custName;
+    String custLastName;
+
+    public void setCustId(int custId)
+    {
+        custId=custId;
+    }
+    public int getCustId()
+    {
+        return custId;
+    }
+
+    public void setCustName(String custName)
+    {
+        custName=custName;
+    }
+    public String getCustName()
+    {
+        return custName;
+    }
+}
